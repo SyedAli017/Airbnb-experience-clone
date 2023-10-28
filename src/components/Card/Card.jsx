857c8827da51f4b5f739f43ab7e2677fc8bccae4
Syed Ali <sydali3103@gmail.com>
2023-10-28 15:32:40 +0530

    implemented the card section

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,21 +2,28 @@ import "./card.css";
 import Star from "/images/star.png";
 
 const Card = (props) => {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
-  
-      <div className="card">
-        <img src={`../images/${props.img}`} className="card-image" />
-        <div className="card-stats">
-          <img src={Star} className="card-star" alt="star" />
-          <span>{props.rating}</span>
-          <span className="gray">({props.reviewCount}) • </span>
-          <span className="gray">{props.location}</span>
-        </div>
-        <p>{props.title}</p>
-        <p>
-          <span className="bold">From ${props.price}</span> / person
-        </p>
+    <div className="card">
+      {badgeText && <div className="card-badge">{badgeText}</div>}
+      <img src={`../images/${props.item.coverImg}`} className="card-image" />
+      <div className="card-stats">
+        <img src={Star} className="card-star" alt="star" />
+        <span className="card-rating">{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
+      <p className="card-title">{props.item.title}</p>
+      <p className="card-details">
+        <span className="card-price">From ${props.item.price}</span> / person
+      </p>
+    </div>
   );
 };
 
